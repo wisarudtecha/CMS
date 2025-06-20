fa918a914545c97ce4cb1d196c57fef9074d0c8e
wisarudtecha <champ028@hotmail.com>
2025-06-20 15:00:57 +0700

    feat: custom components attribute
    - Modify className of Modal
    - Modify className of FileInput

--- src/components/ui/modal/index.tsx
+++ src/components/ui/modal/index.tsx
@@ -54,7 +54,10 @@ export const Modal: React.FC<ModalProps> = ({
     : "relative w-full rounded-3xl bg-white  dark:bg-gray-900";
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center overflow-y-auto modal z-99999">
+    <div
+      // className="fixed inset-0 flex items-center justify-center overflow-y-auto modal z-99999"
+      className={`fixed inset-0 flex items-center justify-center overflow-y-auto modal z-99999 ${isFullscreen ? "p-6 bg-white dark:bg-gray-900" : ""}`}
+    >
       {!isFullscreen && (
         <div
           className="fixed inset-0 h-full w-full bg-gray-400/50 backdrop-blur-[32px]"
