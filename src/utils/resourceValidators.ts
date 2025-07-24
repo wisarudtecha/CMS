59bd55afa0ad048b448f37071ef92d535d74f61d
wisarudtecha <champ028@hotmail.com>
2025-07-24 11:25:33 +0700

    perf: utility function naming
    - Re-structure, and re-name /utils files to reusable in the future.

---
+++ src/utils/resourceValidators.ts
@@ -0,0 +1,9 @@
+// /src/utils/resourceValidators.ts
+export const isImageAvailable = (src: string): Promise<boolean> => {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.onload = () => resolve(true);
+    img.onerror = () => resolve(false);
+    img.src = src;
+  });
+}
